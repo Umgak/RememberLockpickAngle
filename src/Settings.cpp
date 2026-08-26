7581416e32e255d033d03ae824550c0e298e416d
Sayuri <40526330+Umgak@users.noreply.github.com>
2026-08-26 01:18:48 -0500

    Fix crash while parsing settings on Windows On Linux, std::strtoul doesn't throw an exception upon encountering an empty string, and instead just returns null. On Windows, it (correctly) throws a std::invalid_argument which, being unhandled, causes the whole game to crash! It wasn't seen by users because of the xbyak issue, so that's nice I guess. Anyway, catch that exception and also handle the std::out_of_range exception in one go. Why are my commits so wordy?

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -20,9 +20,15 @@ namespace Settings {
 		std::string modConfigPath = std::format(R"(Data\SKSE\Plugins\{}.ini)", RLA_SHORT_NAME);
 		modSettings settings;
 		inicpp::IniManager _ini(modConfigPath);
-		std::string perkString = _ini[RLA_SHORT_NAME].toString("requiredPerk");
-		settings.perkRelId = std::stoul(perkString, nullptr, 0x10); // inicpp does not have any builtin for hexadecimal, convert string->int
-		settings.moduleName = _ini[RLA_SHORT_NAME].toString("modName");
+		try {
+			std::string perkString = _ini[RLA_SHORT_NAME].toString("requiredPerk");
+			if(!perkString.empty()) { // don't even throw the exception in this case. Stop parsing and return empty settings object.
+				settings.perkRelId = std::stoul(perkString, nullptr, 0x10); // inicpp does not have any builtin for hexadecimal, convert string->int
+			}
+			settings.moduleName = _ini[RLA_SHORT_NAME].toString("modName");
+		} catch (...) {
+			_WARNING("[WARNING] Failed to parse config. Falling back to default behavior."); // in case you try to enter the NAME of the perk or something, catch error and return empty
+		}
 		return settings;
 	}
 }
